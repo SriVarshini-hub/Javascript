bb85b7da41147cf498fcd2fa6079adabaf09036d
SriVarshini-hub <srivarshinivivekanandan14@gmail.com>
2025-02-19 19:34:51 +0530

    update

--- If_else.js
+++ If_else.js
@@ -1,12 +1,9 @@
-// let age = prompt("Enter your age:");
-// age = Number(age);
-// if (age != null && age >= 18) {
-//   console.log("you are eligible for vote");
-// }
+let age = prompt("Enter your age: ");
+if (age != null && age >= 18) {
+  console.log("you are eligible for vote");
+}
 
 // let age = prompt("Enter your age:");
-// age = Number(age); // Convert input to a number
-
 // if (age != null && age >= 18) {
 //   console.log("You are eligible to vote");
 // } else {
@@ -32,44 +29,38 @@
 //   console.log(number, "is zero number");
 // }
 
-// let avg=45;               // Example 2
-// if(avg>=90 && avg<=100)
-// {
-// console.log("grade A");
-// }
-// else if(avg>=80 && avg<=89)
-// {
-// console.log("grade B");
-// }
-// else if(avg>=70 && avg<=79)
-// {
-// console.log("grade C");
-// }
-// else {
-// console.log("grade D");
+// let avg = 45; // Example 2
+// if (avg >= 90 && avg <= 100) {
+//   console.log("grade A");
+// } else if (avg >= 80 && avg <= 89) {
+//   console.log("grade B");
+// } else if (avg >= 70 && avg <= 79) {
+//   console.log("grade C");
+// } else {
+//   console.log("grade D");
 // }
 
-let english = 95, // Nested if
-  tamil = 98,
-  maths = 75;
-let total, avg;
-total = english + tamil + maths;
-avg = total / 3;
-console.log("total:" + total);
-console.log("avg:" + avg.toFixed(2));
+// let english = 95, // Nested if
+//   tamil = 98,
+//   maths = 75;
+// let total, avg;
+// total = english + tamil + maths;
+// avg = total / 3;
+// console.log("total:" + total);
+// console.log("avg:" + avg.toFixed(2));
 
-if (english >= 35 && tamil >= 35 && maths >= 35) {
-  console.log("result:pass");
-  if (avg > 90 && avg <= 100) {
-    console.log("grade: A grade");
-  } else if (avg > 80 && avg <= 90) {
-    console.log("grade: B grade");
-  } else if (avg > 70 && avg <= 80) {
-    console.log("grade: C grade");
-  } else {
-    console.log("grade: D grade");
-  }
-} else {
-  console.log("result: Fail");
-  console.log("grade: no grade");
-}
+// if (english >= 35 && tamil >= 35 && maths >= 35) {
+//   console.log("result:pass");
+//   if (avg > 90 && avg <= 100) {
+//     console.log("grade: A grade");
+//   } else if (avg > 80 && avg <= 90) {
+//     console.log("grade: B grade");
+//   } else if (avg > 70 && avg <= 80) {
+//     console.log("grade: C grade");
+//   } else {
+//     console.log("grade: D grade");
+//   }
+// } else {
+//   console.log("result: Fail");
+//   console.log("grade: no grade");
+// }
