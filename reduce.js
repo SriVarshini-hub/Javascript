40cf969f10c47b02959233e5f4426a874a492f1e
SriVarshini-hub <srivarshinivivekanandan14@gmail.com>
2025-04-03 21:18:30 +0530

    reduce flattenedarray

--- reduce.js
+++ reduce.js
@@ -1,1 +1,20 @@
-//Reduce() in javascript:
+//1. Reduce() in javascript:
+// Array.reduce(function (accumulator, currentValue, currentIndex, array) {},
+// initialValue);
+
+// let numbers = [1, 2, 3, 4, 5];
+// let sum = numbers.reduce(
+//   (accumulator, currentValue) => accumulator + currentValue
+// );
+// console.log(sum);
+// 2. Flattening an array:
+let nestedArray = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+];
+
+let flattenedArray = nestedArray.reduce((accumulator, currentValue) =>
+  accumulator.concat(currentValue)
+);
+console.log(flattenedArray);
